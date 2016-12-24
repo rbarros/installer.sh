c15dcd429cc65ad593b8baab85c871e4deaaffac
Ramon Barros <contato@ramon-barros.com>
2016-12-24 15:44:37 -0200

    Remove php5-xml, php5-json and php5-zip install ubuntu 12

--- linux/ubuntu/php-12.sh
+++ linux/ubuntu/php-12.sh
@@ -4,7 +4,7 @@
 
   php_main() {
     debug "Install php $DISTRO $RELEASE"
-    super -v+ $PACKAGE install php5 php5-dev php5-mcrypt php5-common php5-curl php5-cli php5-gd php5-json php5-xml libapache2-mod-php5 php5-zip php-pear build-essential
+    super -v+ $PACKAGE install php5 php5-dev php5-mcrypt php5-common php5-curl php5-cli php5-gd libapache2-mod-php5 libphp-pclzip php-pear build-essential
     super -v+ a2enmod rewrite
   }
 
