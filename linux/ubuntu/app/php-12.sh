3a1891c8a2e5098b82de8a92738dad455ddd2fb9
Ramon Barros <rbarros_@hotmail.com>
2018-06-18 16:22:49 -0300

    Update php-12.sh

--- linux/ubuntu/app/php-12.sh
+++ linux/ubuntu/app/php-12.sh
@@ -4,7 +4,7 @@
 
   php_main() {
     debug "Install php $DISTRO $RELEASE"
-    super -v+ $PACKAGE $PACKAGE_INSTALL php5 php5-dev php5-mcrypt php5-common php5-curl php5-cli php5-gd libapache2-mod-php5 libphp-pclzip php-pear build-essential
+    super -v+ $PACKAGE $PACKAGE_INSTALL php5 php5-dev php5-mcrypt php5-common php5-curl php5-cli php5-gd libapache2-mod-php5 php5-mysql libphp-pclzip php-pear build-essential
     super -v+ a2enmod rewrite
     super -v+ service apache2 restart
   }
