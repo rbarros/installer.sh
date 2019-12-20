8fee95dc3b55d5d4a52237784315c89816e321cb
Ramon Barros <contato@ramon-barros.com>
2019-12-20 10:15:56 -0300

    Update php-18.sh
    Add extension bcmath

--- linux/ubuntu/app/php-18.sh
+++ linux/ubuntu/app/php-18.sh
@@ -4,7 +4,7 @@
 
   php_main() {
     debug "Install php $DISTRO $RELEASE"
-    super -v+ $PACKAGE $PACKAGE_INSTALL php7.2 php7.2-dev php7.2-mbstring php7.2-common php7.2-curl php7.2-cli php7.2-gd php7.2-json php7.2-xml libapache2-mod-php7.2 zip unzip php7.2-zip php7.2-mysql php-pear build-essential #build-dep
+    super -v+ $PACKAGE $PACKAGE_INSTALL php7.2 php7.2-dev php7.2-mbstring php7.2-common php7.2-curl php7.2-cli php7.2-gd php7.2-json php7.2-xml libapache2-mod-php7.2 zip unzip php7.2-zip php7.2-mysql php7.2-bcmath php-pear build-essential #build-dep
     super -v+ a2enmod rewrite
     super -v+ service apache2 restart
   }
