72991ec14095b3f85852408f71699e4bb824948f
Tr4ctor Soluções <ramon@tr4ctor.io>
2025-12-03 11:27:48 -0300

    :hammer: Bug - Instalação do php8.3

--- linux/ubuntu/app/php-24.sh
+++ linux/ubuntu/app/php-24.sh
@@ -4,7 +4,7 @@
 
   php_main() {
     debug "Install php $DISTRO $RELEASE"
-    super -v+ $PACKAGE $PACKAGE_INSTALL php8.3 php8.3-dev php8.3-mbstring php8.3-common php8.3-curl php8.3-cli php8.3-gd php8.3-xml php8-zip php8.3-mysql php8.3-bcmath php8.3-readline php-pear libapache2-mod-php8.3 zip unzip build-essential
+    super -v+ $PACKAGE $PACKAGE_INSTALL php8.3 php8.3-dev php8.3-mbstring php8.3-common php8.3-curl php8.3-cli php8.3-gd php8.3-xml php8.3-zip php8.3-mysql php8.3-bcmath php8.3-readline php-pear libapache2-mod-php8.3 zip unzip build-essential
     super -v+ a2enmod rewrite
     super -v+ service apache2 restart
   }
